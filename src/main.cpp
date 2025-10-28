2d03d16611c04c915fa0e0547996d7b20e5f1878
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-10-28 16:36:01 -0500

    chore: updated main to use new basilisk syntax

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,18 @@
 
 int main() {
     Engine* engine = new Engine(800, 800, "Basilisk");
-    Scene* scene = new Scene(engine);
     Scene2D* scene2D = new Scene2D(engine);
 
     // Data for making node
-    Mesh* cube = new Mesh("models/cube.obj");
     Mesh* quad = new Mesh("models/quad.obj");
     Image* image = new Image("textures/container.jpg");
     Texture* texture = new Texture(image);
         
-    Node* parent = new Node(scene, scene->getShader(), cube, texture);
-    Node* node = new Node(parent, scene->getShader(), cube, texture);
-    new Node(node, scene->getShader(), cube, texture);
-    Node* clone = node;
-    clone = std::move(parent);
-
-    Node2D* square = new Node2D(scene2D, scene2D->getShader(), quad, texture);
+    Node2D* square = new Node2D(scene2D, quad, texture);
 
     // Main loop continues as long as the window is open
     while (engine->isRunning()) {
         engine->update();
-
-        scene->update();
-        scene->render();
 
         scene2D->update();
         scene2D->render();
@@ -35,10 +24,7 @@ int main() {
     // Free memory allocations
     delete image;
     delete texture;
-    delete cube;
-    delete node;
     delete quad;
-    delete scene;
     delete scene2D;
     delete engine;
 }
