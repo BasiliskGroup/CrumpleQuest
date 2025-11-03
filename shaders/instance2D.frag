27f6bd7b6804583474b3fbab333739355185b1f6
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-11-03 15:30:50 -0600

    Fixed shaders and updated main to fix basilisk

---
+++ shaders/instance2D.frag
@@ -0,0 +1,36 @@
+#version 330 core
+
+struct textArray {
+    sampler2DArray array;
+};
+
+struct MaterialData {
+    vec3 color;
+
+    uint albedoArray;
+    uint albedoIndex;
+    uint normalArray;
+    uint normalIndex;
+
+    float roughness;
+    float subsurface;
+    float sheen;
+    float sheenTint;
+    float anisotropic;
+    float specular;
+    float metallicness;
+    float clearcoat;
+    float clearcoatGloss;
+};
+
+in vec2 uv;
+flat in MaterialData material;
+
+uniform sampler2D uTexture;
+uniform textArray textureArrays[4];
+
+out vec4 fragColor;
+
+void main() {
+    fragColor = texture(textureArrays[material.albedoArray].array, vec3(uv, material.albedoIndex));
+} 
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@ int main() {
 
     // Data for making node
     Mesh* quad = new Mesh("models/quad.obj");
-    Image* image = new Image("textures/container.jpg");
-    Texture* texture = new Texture(image);
+    Image* image = new Image("textures/man.png");
+    Material* material1 = new Material({1.0, 1.0, 0.0}, image);
         
-    Node2D* square = new Node2D(scene2D, { .mesh=quad, .texture=texture });
+    Node2D* square = new Node2D(scene2D, { .mesh=quad, .material=material1 });
 
     // Main loop continues as long as the window is open
     while (engine->isRunning()) {
         engine->update();
 
-        scene2D->update();
+        scene2D->update(1 / 60);
         scene2D->render();
 
         engine->render();
@@ -23,7 +23,7 @@ int main() {
 
     // Free memory allocations
     delete image;
-    delete texture;
+    delete material1;
     delete quad;
     delete scene2D;
     delete engine;
