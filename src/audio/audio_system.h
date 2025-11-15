e9abcc85c2f6b4f628764e518f0717cb4230c1dd
hanna <hanna.marie.harmon@gmail.com>
2025-11-15 11:22:23 -0600

    Change: move miniaudio files to include directory

--- src/audio/audio_system.h
+++ src/audio/audio_system.h
@@ -1,7 +1,7 @@
 #ifndef AUDIO_AUDIO_SYSTEM_H_
 #define AUDIO_AUDIO_SYSTEM_H_
 
-#include "miniaudio.h"
+#include "miniaudio/miniaudio.h"
 #include <memory>
 #include <string>
 #include <unordered_map>
