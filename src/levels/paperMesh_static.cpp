1c8b6173349872ad3f10a9ecd9ad10cb4307eda4
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-12-05 20:28:30 -0600

    Demo code for obstacles

--- src/levels/paperMesh_static.cpp
+++ src/levels/paperMesh_static.cpp
@@ -1,4 +1,5 @@
 #include "levels/levels.h"
+#include "util/maths.h"
 
 std::unordered_map<std::string, std::function<std::vector<UVRegion>()>> PaperMesh::obstacleTemplates;
 
@@ -11,7 +12,18 @@ void PaperMesh::generateTemplates(Game* game) {
 
     obstacleTemplates["squareMiddle"] = [game]() { 
         std::vector<UVRegion> obst;
-        obst.push_back({ game->getMesh("quad"), { 0, 0, 0 }, { 3, 3 }, true });
+        obst.push_back({ game->getMesh("quad"), { 0, 0, 0 }, { 12, 9 }, true });
+        return obst; 
+    };
+
+    obstacleTemplates["notebook1_level1"] = [game]() { 
+        std::vector<UVRegion> obst;
+
+        // how to make a single line
+        std::pair<glm::vec3, glm::vec2> data = connectSquare(vec2{ 0, 0 }, vec2{ 12, 9 });
+        obst.push_back({ game->getMesh("quad"), data.first, data.second, true });
+
+        
         return obst; 
     };
 }
